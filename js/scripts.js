59b85a7afaa37c001310d2081be26a49799d0eb0
Henrik Johansson <hencjo@gmail.com>
2014-11-27 16:13:28 +0100

    From Anders.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Anders on 2014-11-26.
+ */
+
+
+$( document ).ready(function() {
+    // SVG fallback
+    // toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script#update
+    !function () {
+        function supportsSVG() {
+            return !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect
+        };
+        if (supportsSVG()) document.documentElement.className += ' svg';
+    }();
+
+    //Super easy scroller
+    $('.scroller').on("click", function () {
+        var target = ($(this).attr('href'));
+        $("html, body").animate({
+            scrollTop: $(target).offset().top //- 20
+        }, 400);
+        return false;
+    });
+});
