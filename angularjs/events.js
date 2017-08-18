73d92264ea7323a28ad05f484b56d3312dd34f30
Fredrik Wendt <fredrik@wendt.se>
2017-08-18 12:01:05 +0200

    Woody Weeks added

--- angularjs/events.js
+++ angularjs/events.js
@@ -1,5 +1,6 @@
 function EventsController($scope) {
 	var events = [
+		{name: "Mob Programming With Woody Zuil", date: "2017-09-18", "time": "until 2017-09-29", where: "Göteborg", type: "Course", link: "/course-mobprogramming.html"},
 		{name: "Introduktion till agila metoder - Lean, Scrum & Kanban", date: "2015-02-13", time: "09:00 - 17:00", where: "Drakegatan 7, Göteborg", type: "Course", link: "http://informator.se/boka?ArticleNr=T1420&CourseDateID=14162"},
 		{name: "Kanban - Practical Workshop", date: "2015-02-19", time: "09:00 - 17:00", where: "Drakegatan 7, Göteborg", type: "Course", link: "http://informator.se/boka?ArticleNr=T1425&CourseDateID=14171#conversion-0"},
 		{name: "Introduktion till agila metoder - Lean, Scrum & Kanban", date: "2015-02-02", time: "09:00 - 17:00", where: "Karlavägen 108, Stockholm", type: "Course", link: "http://informator.se/boka?ArticleNr=T1420&CourseDateID=14161"},
